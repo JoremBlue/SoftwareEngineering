9bf2ab15395cf013494647ce282a5f5876b7d4ff
teachemeeeeeeeeeeee <135201717+teachemeeeeeeeeeeee@users.noreply.github.com>
2025-02-10 08:45:36 +0800

    Delete AccountState.java

--- AccountState.java
+++
@@ -1,7 +0,0 @@
-public interface AccountState {
-    public void deposit(Account account, double amount);
-    public void withdraw(Account account, double amount);
-    public void suspend(Account account);
-    public void activate(Account account);
-    public void close(Account account);
-}
